bc519361e03fd7cf178109bc2b783c9d8e0b5537
Gigabyte <amriliss@yandex.ru>
2023-07-12 22:37:03 +0300

    Revert "feature: выполнить требования ТЗ13"
    This reverts commit 5804c57c72495102da14f643246552fa612d0143.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,27 +1,7 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.*;
-import ru.practicum.shareit.request.ItemRequest;
-import ru.practicum.shareit.user.model.User;
-
-
-@Getter
-@Setter
-@RequiredArgsConstructor
-@ToString
-@EqualsAndHashCode
+/**
+ * TODO Sprint add-controllers.
+ */
 public class Item {
-    private long id;
-    private String name;
-    private String description;
-    private Boolean available;
-    private User owner;
-    private ItemRequest request;
-
-    public Item(long id, String name, String description, boolean available) {
-        this.setId(id);
-        this.setName(name);
-        this.setDescription(description);
-        this.setAvailable(available);
-    }
 }

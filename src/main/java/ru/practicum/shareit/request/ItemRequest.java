5804c57c72495102da14f643246552fa612d0143
Gigabyte <amriliss@yandex.ru>
2023-07-12 20:51:22 +0300

    feature: выполнить требования ТЗ13

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.request;
 
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Entity
+@Table(name = "requests")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class ItemRequest {
+    private long id;
+    private String description;
+    private User requestor;
+    private LocalDateTime created;
 }

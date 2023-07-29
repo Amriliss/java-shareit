cda8552771219a3b7e2642ef8c39bcdd295fe59d
Gigabyte <amriliss@yandex.ru>
2023-07-29 13:27:56 +0300

    code style2

--- src/main/java/ru/practicum/shareit/comment/service/CommentService.java
+++ src/main/java/ru/practicum/shareit/comment/service/CommentService.java
@@ -5,9 +5,6 @@ import ru.practicum.shareit.comment.dto.CommentDto;
 import java.util.List;
 
 public interface CommentService {
-
     CommentDto comment(Long userId, Long itemId, CommentDto commentDto);
-
     List<CommentDto> commentDtos(Long itemId);
-
 }

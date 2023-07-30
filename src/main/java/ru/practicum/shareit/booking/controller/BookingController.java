1bdf6d420772b8c338e5c1819c75e3eb7f8f9e76
Gigabyte <amriliss@yandex.ru>
2023-07-30 15:24:33 +0300

    code style 4

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -13,34 +13,34 @@ import java.util.List;
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
 public class BookingController {
-    private final String X_SHARER_USER_ID = "X-Sharer-User-Id";
+    private final String xSharerUserId = "X-Sharer-User-Id";
     private final BookingService bookingService;
 
     @PostMapping
-    public BookingInfoDto create(@RequestHeader(X_SHARER_USER_ID) Long userId, @Valid @RequestBody BookingDto bookingDto) {
+    public BookingInfoDto create(@RequestHeader(xSharerUserId) Long userId, @Valid @RequestBody BookingDto bookingDto) {
         return bookingService.create(userId, bookingDto);
     }
 
     @PatchMapping("/{bookingId}")
-    public BookingInfoDto approve(@RequestHeader(X_SHARER_USER_ID) Long userId, @PathVariable Long bookingId,
+    public BookingInfoDto approve(@RequestHeader(xSharerUserId) Long userId, @PathVariable Long bookingId,
                                   @RequestParam Boolean approved
     ) {
         return bookingService.approve(userId, bookingId, approved);
     }
 
     @GetMapping("/{bookingId}")
-    public BookingInfoDto get(@RequestHeader(X_SHARER_USER_ID) Long userId, @PathVariable Long bookingId) {
+    public BookingInfoDto get(@RequestHeader(xSharerUserId) Long userId, @PathVariable Long bookingId) {
         return bookingService.get(userId, bookingId);
     }
 
     @GetMapping
-    public List<BookingInfoDto> get(@RequestHeader(X_SHARER_USER_ID) Long userId,
+    public List<BookingInfoDto> get(@RequestHeader(xSharerUserId) Long userId,
                                     @RequestParam(defaultValue = "ALL", required = false) String state) {
         return bookingService.get(userId, state);
     }
 
     @GetMapping("/owner")
-    public List<BookingInfoDto> getByOwner(@RequestHeader(X_SHARER_USER_ID) Long userId,
+    public List<BookingInfoDto> getByOwner(@RequestHeader(xSharerUserId) Long userId,
                                            @RequestParam(defaultValue = "ALL", required = false) String state) {
         return bookingService.getByOwner(userId, state);
     }
